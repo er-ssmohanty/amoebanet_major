32a8ca97bedbea57f793998a1f114dc6f181e7e2
er-ssmohanty <pie2001@protonmail.com>
2023-02-14 13:23:09 +0530

    changed the functions to be more compatible

--- src/Amoeaba/Reduction_cell.py
+++ src/Amoeaba/Reduction_cell.py
@@ -1,20 +1,20 @@
-from Amoeaba import ImportAll
+from Amoeaba.ImportAll import *
 #from src.Amoeaba import ImportAll
 
-def reduction_cell(cell_base_depth, cell_growth_rate, reduction_cell_multiplier, filters_factor,
+def reduction_cell(input_tensor, cell_base_depth, cell_growth_rate, reduction_cell_multiplier, filters_factor,
                    weight_decay, dropout_rate, reduction_cell_groups):
     
     def cell(inputs, filters, strides):
         x = Conv2D(filters * filters_factor, (1, 1), padding='same', use_bias=False,
-                   kernel_regularizer=l2(weight_decay))(inputs)
+                   kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay))(inputs)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = Conv2D(filters * filters_factor, (3, 3), strides=strides, padding='same',
-                   use_bias=False, kernel_regularizer=l2(weight_decay))(x)
+                   use_bias=False, kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay))(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = Conv2D(filters * reduction_cell_multiplier, (1, 1), padding='same',
-                   use_bias=False, kernel_regularizer=l2(weight_decay))(x)
+                   use_bias=False, kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay))(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = MaxPooling2D((3, 3), strides=strides, padding='same')(x)
@@ -25,7 +25,7 @@ def reduction_cell(cell_base_depth, cell_growth_rate, reduction_cell_multiplier,
     cell_outputs = []
     for i in range(reduction_cell_groups):
         strides = 2 if i == 0 else 1
-        x = cell(inputs, filters, strides)
+        x = cell(input_tensor, filters, strides)
         cell_outputs.append(x)
         filters *= cell_growth_rate
         
